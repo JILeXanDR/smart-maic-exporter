15bf01f916d062e6267275a158e0cc5efa84117a
Oleksandr Shtovba <jilexandr@gmail.com>
2024-12-01 13:32:45 +0200

    use Gauge again

--- smart_maic_exporter.py
+++ smart_maic_exporter.py
@@ -4,7 +4,7 @@ import sys
 
 import requests
 from flask import Flask, Response
-from prometheus_client import CollectorRegistry, Gauge, Counter, generate_latest
+from prometheus_client import CollectorRegistry, Gauge, generate_latest
 
 # Configure Logging
 logging.basicConfig(
@@ -37,6 +37,7 @@ registry = CollectorRegistry()
 
 # Define Prometheus Metrics with a common prefix
 metric_prefix = "smart_maic_"
+
 metrics = {
     # Gauges
     "voltage": Gauge(f"{metric_prefix}voltage", "Voltage (V)", ["line"], registry=registry),
@@ -44,10 +45,8 @@ metrics = {
     "power_factor": Gauge(f"{metric_prefix}power_factor", "Power Factor", ["line"], registry=registry),
     "frequency": Gauge(f"{metric_prefix}frequency", "Frequency (Hz)", ["line"], registry=registry),
     "temperature": Gauge(f"{metric_prefix}temperature", "Device Temperature (°C)", registry=registry),
-
-    # Counters
-    "energy": Counter(f"{metric_prefix}energy", "Energy (Wh)", ["line"], registry=registry),
-    "total_energy": Counter(f"{metric_prefix}total_energy", "Total Energy (Wh)", registry=registry),
+    "energy": Gauge(f"{metric_prefix}energy", "Energy (Wh)", ["line"], registry=registry),
+    "total_energy": Gauge(f"{metric_prefix}total_energy", "Total Energy (Wh)", registry=registry),
 }
 
 
@@ -55,7 +54,7 @@ metrics = {
 def fetch_and_update_metrics():
     logging.info("Fetching data from the Smart Maic device...")
     try:
-        response = requests.get(DATA_SOURCE_URL, timeout=10)
+        response = requests.get(DATA_SOURCE_URL, timeout=3)
         response.raise_for_status()
         data = response.json()
 
@@ -70,11 +69,11 @@ def fetch_and_update_metrics():
             metrics["current"].labels(line=line).set(float(device_data[f"A{line}"]["value"]))  # Last current
             metrics["power_factor"].labels(line=line).set(float(device_data[f"PF{line}"]["value"]))  # Last power factor
             metrics["frequency"].labels(line=line).set(float(device_data[f"Fr{line}"]["value"]))  # Last frequency
-            metrics["energy"].labels(line=line).set(float(device_data[f"Wh{line}"]["value"]))  # Total energy
+            metrics["energy"].labels(line=line).set(float(device_data[f"Wh{line}"]["value"]))  # Last total energy
 
         # Update general metrics
-        metrics["temperature"].set(float(device_data["T"]["value"]))  # Last temperature
-        metrics["total_energy"].set(float(device_data["TWh"]["value"]))  # Total energy
+        metrics["temperature"].set(float(device_data["T"]["value"]))  # Last temperature of device itself
+        metrics["total_energy"].set(float(device_data["TWh"]["value"]))  # Last total energy
 
         logging.info("Successfully updated metrics.")
 
